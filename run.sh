272c079b02da9e74c064d55e7e4e1a4950842f7e
Otto Fernandes <48037630+ottusp@users.noreply.github.com>
2022-01-30 18:14:38 -0300

    Add aditional zsh plugins

--- run.sh
+++ run.sh
@@ -4,7 +4,14 @@
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
+# Install zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install p10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
 # Create symlinks on home directory
 ln -s ~/.dotfiles/.aliases ~/.aliases
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
