ad6cc94270987926b75df5c03392e2dcf93873c0
Otto Fernandes <48037630+ottusp@users.noreply.github.com>
2022-01-30 19:46:50 -0300

    Upgrade node

--- node_setup.sh
+++ node_setup.sh
@@ -2,3 +2,13 @@
 mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 echo "export PATH=~/.npm-global/bin:$PATH" >> ~/.profile
+
+# Install n
+npm i -g n
+
+# Upgrade node
+sudo n lts
+
+# Upgrade npm
+npm i -g npm
+
